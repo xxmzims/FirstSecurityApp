b091f8c36016eb1f78fef56b9406fb91dcedf088
Maksim Ugrinovitch <usmaksim52@gmail.com>
2025-02-05 11:06:34 +0300

    Delete Auth Provider. Start using UserDetailsService for Auth

--- src/main/java/ru/ugrinovich/FirstSecurityApp/config/SecurityConfig.java
+++ src/main/java/ru/ugrinovich/FirstSecurityApp/config/SecurityConfig.java
@@ -1,24 +1,30 @@
 package ru.ugrinovich.FirstSecurityApp.config;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfiguration;
-import ru.ugrinovich.FirstSecurityApp.security.AuthProviderImpl;
+import org.springframework.security.crypto.password.NoOpPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import ru.ugrinovich.FirstSecurityApp.services.PersonDetailsService;
 
 
 // в этом классе настраивается наша конфигурация Security.
 @EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfiguration {
 
-    private  final AuthProviderImpl authProvider;
+    private final PersonDetailsService personDetailsService;
 
-    // передаем в конструктор класс AuthProvider, этот класс отвечает за проведение аутентификации
-    public SecurityConfig(AuthProviderImpl authProvider) {
-        this.authProvider = authProvider;
+    public SecurityConfig(PersonDetailsService personDetailsService) {
+        this.personDetailsService = personDetailsService;
     }
 
     // передаем наш AuthProvider в специальный класс,
-    protected void configure(AuthenticationManagerBuilder auth){
-        auth.authenticationProvider(authProvider);
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.userDetailsService(personDetailsService);
+    }
+    @Bean
+    public PasswordEncoder getpasswordEncoder(){
+        return NoOpPasswordEncoder.getInstance();
     }
 }
